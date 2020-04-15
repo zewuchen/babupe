b07246dcd6b91c47827c1c81da1f4ac099f5f312
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-04-15 14:39:52 -0300

    add collectionView with bubble design

---
+++ Babupe/View/BubbleCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BubbleCollectionViewCell.swift
+//  Babupe
+//
+//  Created by Zewu Chen on 15/04/20.
+//  Copyright © 2020 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+class BubbleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bubbleImage: UIImageView!
+}
